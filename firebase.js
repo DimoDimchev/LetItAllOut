144a6b0bb245312b8d6f8f9c344847a765645cab
Dimo Dimchev <ddimchev7@gmail.com>
2021-04-12 22:27:42 +0300

    Created base files

---
+++ firebase.js
@@ -0,0 +1,21 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDXo3ZI0V_2EsWdVs9oKbqmKG5qKtLb17M",
+    authDomain: "blog-a13c9.firebaseapp.com",
+    projectId: "blog-a13c9",
+    databaseURL: "https://blog-a13c9-default-rtdb.firebaseio.com/",
+    storageBucket: "blog-a13c9.appspot.com",
+    messagingSenderId: "805228937519",
+    appId: "1:805228937519:web:7caab3b7d28aa82830a1da"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+let auth = firebase.auth();
+let baseURI = "https://blog-a13c9-default-rtdb.firebaseio.com/";
+
+let email = document.getElementById('email');
+let password = document.getElementById('password');
+let registerBtn = document.getElementById('registerBtn');
+
+export {auth, email, password, registerBtn}
